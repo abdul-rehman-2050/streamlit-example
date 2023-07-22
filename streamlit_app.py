3c081b498e94cdad603cce922eddb7f8cf2b386f
Abdul Rehman <supermind2002@gmail.com>
2023-07-23 01:16:46 +0500

    cv test new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,59 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 import cv2
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from PIL import Image
+import numpy as np
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+def detect_faces(our_image):
+    img = np.array(our_image.convert('RGB'))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangle around the faces
+    name='Unknown'
+    for (x, y, w, h) in faces:
+        # To draw a rectangle in a face
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        id, uncertainty = rec.predict(gray[y:y + h, x:x + w])
+        print(id, uncertainty)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        if (uncertainty< 53):
+            if (id == 1 or id == 3 or id == 5):
+                name = "Nachiketa"
+                cv2.putText(img, name, (x, y + h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2.0, (0, 0, 255))
+        else:
+            cv2.putText(img, 'Unknown', (x, y + h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2.0, (0, 0, 255))
+
+
+    return img
+def main():
+    """Face Recognition App"""
+
+    st.title("Streamlit Tutorial")
+
+    html_temp = """
+    <body style="background-color:red;">
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Face Recognition WebApp</h2>
+    </div>
+    </body>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    image_file = st.file_uploader("Upload Image", type=['jpg', 'png', 'jpeg'])
+    if image_file is not None:
+        our_image = Image.open(image_file)
+        st.text("Original Image")
+        st.image(our_image)
+
+    if st.button("Recognise"):
+        result_img= detect_faces(our_image)
+        st.image(result_img)
+
+
+if __name__ == '__main__':
+    main()
